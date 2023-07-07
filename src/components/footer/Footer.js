a0593c94deaae37a28f6351232a5db9e5eecc516
V1v3kShah <94179705+Vivek-C-Shah@users.noreply.github.com>
2023-07-07 15:14:54 +0530

    full done

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,22 @@
+import React, {useContext} from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Footer() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made with ❤️ by Vivek Shah")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a href="https://github.com/Vivek-C-Shah">The Cipher Vivek</a>
+        </p>
+      </div>
+    </Fade>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,380 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "The Cipher Vivek",
+  title: "Hi all, I'm Vivek Shah",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Viewjs / Ejs and some other cool libraries and frameworks."
+  ),
+  resumeLink: `${process.env.RESUME}`, // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/Vivek-C-shah",
+  linkedin: "https://www.linkedin.com/in/the-cipher-vivek/",
+  gmail: "vivekchiragshah2004@gmail.com",
+  gitlab: "https://gitlab.com/Vivek-C-Shah",
+  medium: "https://medium.com/@VivekShah",
+  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  instagram: "https://www.instagram.com/shhh_vivek/",
+  twitter: "https://twitter.com/ShVivek25",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji("⚡ Develop highly Logical Back-end for your web applications"),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Pandit Deendayal Energy University",
+      logo: require("./assets/images/unilogo.png"),
+      subHeader:
+        "Bachelor of Technology in Information and Communication Technology",
+      duration: "July 2021 - Present"
+      // desc: "Participated in the research of XXX and published 3 papers.",
+      // descBullets: [
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      // ]
+    }
+    //   {
+    //     schoolName: "Stanford University",
+    //     logo: require("./assets/images/stanfordLogo.png"),
+    //     subHeader: "Bachelor of Science in Computer Science",
+    //     duration: "September 2013 - April 2017",
+    //     desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+    //     descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    //   }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Backend",
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Developer Intern",
+      company: "TechInnovate Mobility",
+      companylogo: require("./assets/images/techinnovate.jpeg"),
+      date: "September 2022 - November 2022",
+      desc: "Developed a full stack web application for the company to manage their data and for the marketting of sales.",
+      descBullets: [
+        "Developed Frontend with ReactJS and Backend with NodeJS",
+        "https://techinnovatemobility.com/"
+      ]
+    },
+    {
+      role: "Software Developer Intern",
+      company: "Marwiz Tech Pvt. Ltd.",
+      companylogo: require("./assets/images/marwiz.jpeg"),
+      date: "June 2023 - August 2023",
+      desc: "Learned about the working of the company and developed a full stack web application for the company to manage their customers and their data."
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Cybertag",
+      companylogo: require("./assets/images/cybertag.png"),
+      date: "June 2023 - August 2023",
+      desc: "Learned about the working of the company and developed a full stack web application for the company's clients to manage their customers and their data."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/Effitto.png"),
+      projectName: "Effitto",
+      projectDesc: "A Rental MarketPlace for the people of Ahmedabad, India",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://effitto.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    }
+    // {
+    //   image: require("./assets/images/nextuLogo.webp"),
+    //   projectName: "Nextu",
+    //   projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    //   footerLink: [
+    //     {
+    //       name: "Visit Website",
+    //       url: "http://nextu.se/"
+    //     }
+    //   ]
+    // }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "DotSlash 6.0 Hackathon Finalist",
+      subtitle:
+        "DotSlash is a 36 hour hackathon organized by Coding Blocks for students all over India. I was selected as a finalist for my project 'BSCM - Blockchain Supply Chain Management System' and got a chance to present my project in front of the judges.",
+      image: require("./assets/images/dotslash.png"),
+      imageAlt: "Dotsalsh 6.0",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://certificate.givemycertificate.com/c/dd4e026b-e56c-463e-a944-e985c68ad6ba"
+        }
+      ]
+    },
+    {
+      title: "Certified Ethical Hacker (CEH) - EC-Council",
+      subtitle:
+        "Skills: Digital Forensics · Ethical Hacking · Penetration Testing · Cybersecurity · Information Security",
+      image: require("./assets/images/eccouncil.png"),
+      imageAlt: "EC-Counsil Logo",
+      footerLink: [
+        {
+          name: "Certificate",
+          url: "https://www.coursera.org/account/accomplishments/certificate/T5NJ9GXTPKJD"
+        }
+      ]
+    },
+
+    {
+      title: "Penetration Testing, Incident Response & Digital Forensics - IBM",
+      subtitle:
+        "Completed Certifcation from IBM for Penetration Testing, Incident Response & Digital Forensics.",
+      image: require("./assets/images/ibm.png"),
+      imageAlt: "IBM Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/certificate/SW9ACURR9W3S"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+91-7383123796",
+  email_address: "vivekchiragshah2004@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "ShVivek25", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
