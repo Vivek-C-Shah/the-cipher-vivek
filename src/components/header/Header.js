fc993012c4d3d1178751bb3722838d1d741b0377
V1v3kShah <94179705+Vivek-C-Shah@users.noreply.github.com>
2023-07-20 14:49:44 +0530

    added github stats

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -51,7 +51,7 @@ function Header() {
           )}
           {viewOpenSource && (
             <li>
-              <a href="#opensource">Open Source</a>
+              <a href="#opensource">Projects</a>
             </li>
           )}
           {viewAchievement && (
